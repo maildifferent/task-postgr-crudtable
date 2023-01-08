34f8d01daf708d8f8dd0fe1e070df1a2c89c161f
maildifferent <maildifferent@mail.ru>
2023-01-08 19:16:13 +0300

    some changes

--- src/share/kbserver.ts
+++ src/share/kbserver.ts
@@ -1,3 +1,4 @@
+import { CONFIG } from '../config.js'
 import { ApplicationOptions } from './application.js'
 import { DriverApiResponseT } from './driver_api_response.js'
 import { ErrorCustomType, ErrorCustomUnclassified } from './error.js'
@@ -13,7 +14,7 @@ export interface KbServerRequestI {
 }
 
 export const kbServer = {
-  url: 'http://localhost:8080' as const,
+  url: CONFIG.isProduction ? process.env['KBSERVER_URL'] : 'http://localhost:8080' as const,
 
   createHeaders({ appOptions }: ApplicationOptions): HeadersInit {
     const headers: HeadersInit = { 'Content-Type': 'application/json' }
